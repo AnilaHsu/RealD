a2e7ab69275a6bddcd3eb511f84f617da70c4861
anilahsu <sss880128gmail.com>
2022-10-18 18:58:19 +0800

    feat: add routes to route all pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
-import './App.css';
+import Header from './components/header/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/footer/Footer';
+import Home from './page/home/Home';
+import Technology from './page/technology/Technology';
+import Business from './page/business/Business';
+import Science from './page/science/Science';
+import Sports from './page/sports/Sports';
+import Health from './page/health/Health';
+import Entertainment from './page/entertainment/Entertainment';
+
 
 function App(): JSX.Element {
   return (
-    <div className="App">
+    <>
+     <BrowserRouter>
+        <Header />
 
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/technology" element={<Technology />} />
+          <Route path="/business" element={<Business />} />
+          <Route path="/science" element={<Science />} />
+          <Route path="/sports" element={<Sports />} />
+          <Route path="/health" element={<Health />} />
+          <Route path="/entertainment" element={<Entertainment />} />
+          
+    
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
