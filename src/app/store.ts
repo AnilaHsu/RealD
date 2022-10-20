8f71eeed23be9d8f8e16e482cfbe0a685dc68641
anilahsu <sss880128gmail.com>
2022-10-20 16:04:58 +0800

    feat: add reducer, action and thunk handle news data

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import newsReducer from "./newsSlice" 
 
-export const store = configureStore({
-  reducer: {
-
-  },
+const rootReducer = combineReducers({
+    news: newsReducer
 })
 
-export type RootState = ReturnType<typeof store.getState>
+export const store = configureStore({
+  reducer: rootReducer
+})
+
+export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof store.dispatch
