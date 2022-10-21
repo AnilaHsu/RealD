a74e3d261c32acf7281254d94779e1d6436ceb70
anilahsu <sss880128gmail.com>
2022-10-21 17:12:43 +0800

    feat: update Header layout,and country to countryCode

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,120 +6,124 @@ import { getNewsData, selectCat } from "../../app/newsSlice";
 
 export function Header(): JSX.Element {
   const dispatch = useAppDispatch();
-  const country = useAppSelector((state) => state.news.country);
-
+  const countryCode = useAppSelector((state) => state.news.countryCode);
+  console.log(countryCode)
+  
   return (
     <header className="header">
       <h1 className="logo">
         <img className="logo-img" src={logo} alt="logo" />
-        <Link
-          className="logo-text"
-          to="/"
-          onClick={() => {
-            dispatch(selectCat(""));
-            dispatch(
-              getNewsData({
-                category: "",
-                country: country,
-              })
-            );
-          }}
-        >
-          RealD
-        </Link>
+        RealD news
       </h1>
-      <nav className="menu">
-        <Link
-          className="menu-item"
-          to="/technology"
-          onClick={() => {
-            dispatch(selectCat("technology"));
-            dispatch(
-              getNewsData({
-                category: "technology",
-                country: country,
-              })
-            );
-          }}
-        >
-          technology
-        </Link>
-        <Link
-          className="menu-item"
-          to="/business"
-          onClick={() => {
-            dispatch(selectCat("business"));
-            dispatch(
-              getNewsData({
-                category: "business",
-                country: country,
-              })
-            );
-          }}
-        >
-          business
-        </Link>
-        <Link
-          className="menu-item"
-          to="/science"
-          onClick={() => {
-            dispatch(selectCat("science"));
-            dispatch(
-              getNewsData({
-                category: "science",
-                country: country,
-              })
-            );
-          }}
-        >
-          science
-        </Link>
-        <Link
-          className="menu-item"
-          to="/sports"
-          onClick={() => {
-            dispatch(selectCat("sports"));
-            dispatch(
-              getNewsData({
-                category: "sports",
-                country: country,
-              })
-            );
-          }}
-        >
-          sports
-        </Link>
-        <Link
-          className="menu-item"
-          to="/health"
-          onClick={() => {
-            dispatch(selectCat("health"));
-            dispatch(
-              getNewsData({
-                category: "health",
-                country: country,
-              })
-            );
-          }}
-        >
-          health
-        </Link>
-        <Link
-          className="menu-item"
-          to="/entertainment"
-          onClick={() => {
-            dispatch(selectCat("entertainment"));
-            dispatch(
-              getNewsData({
-                category: "entertainment",
-                country: country,
-              })
-            );
-          }}
-        >
-          entertainment
-        </Link>
-      </nav>
+      <div className="sub-header">
+        <nav className="menu">
+          <Link
+            className="menu-item"
+            to="/"
+            onClick={() => {
+              dispatch(selectCat(""));
+              dispatch(
+                getNewsData({
+                  category: "",
+                  country: countryCode,
+                })
+              );
+            }}
+          >
+            home
+          </Link>
+          <Link
+            className="menu-item"
+            to="/technology"
+            onClick={() => {
+              dispatch(selectCat("technology"));
+              dispatch(
+                getNewsData({
+                  category: "technology",
+                  country: countryCode,
+                })
+              );
+            }}
+          >
+            technology
+          </Link>
+          <Link
+            className="menu-item"
+            to="/business"
+            onClick={() => {
+              dispatch(selectCat("business"));
+              dispatch(
+                getNewsData({
+                  category: "business",
+                  country: countryCode,
+                })
+              );
+            }}
+          >
+            business
+          </Link>
+          <Link
+            className="menu-item"
+            to="/science"
+            onClick={() => {
+              dispatch(selectCat("science"));
+              dispatch(
+                getNewsData({
+                  category: "science",
+                  country: countryCode,
+                })
+              );
+            }}
+          >
+            science
+          </Link>
+          <Link
+            className="menu-item"
+            to="/sports"
+            onClick={() => {
+              dispatch(selectCat("sports"));
+              dispatch(
+                getNewsData({
+                  category: "sports",
+                  country: countryCode,
+                })
+              );
+            }}
+          >
+            sports
+          </Link>
+          <Link
+            className="menu-item"
+            to="/health"
+            onClick={() => {
+              dispatch(selectCat("health"));
+              dispatch(
+                getNewsData({
+                  category: "health",
+                  country: countryCode,
+                })
+              );
+            }}
+          >
+            health
+          </Link>
+          <Link
+            className="menu-item"
+            to="/entertainment"
+            onClick={() => {
+              dispatch(selectCat("entertainment"));
+              dispatch(
+                getNewsData({
+                  category: "entertainment",
+                  country: countryCode,
+                })
+              );
+            }}
+          >
+            entertainment
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 }
